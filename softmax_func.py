a10143208f5b0dcfee66a655355290cd0b8ab0fe
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2017-02-12 11:07:20 +0900

    softmax

--- softmax_func.py
+++ softmax_func.py
@@ -8,6 +8,12 @@ def softmax(a):
     return y
 
 
-a = np.array([0.3, 2.9, 4.0])
+a = np.array([1010, 1000, 990])
 
 print(softmax(a))
+
+c = np.max(a)
+print(a - c)
+
+y = np.exp(a - c) / np.sum(np.exp(a - c))
+print(y)
