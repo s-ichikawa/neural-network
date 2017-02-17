5c49d8e71f273bafc8cfe3855a11bf34abb45d8a
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2017-02-17 23:13:27 +0900

    neuralnet_mnist

--- sigmoid_func.py
+++ sigmoid_func.py
@@ -6,8 +6,8 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
-x = np.arange(-5.0, 5.0, 0.1)
-y = sigmoid(x)
+# x = np.arange(-5.0, 5.0, 0.1)
+# y = sigmoid(x)
 
 # plt.plot(x, y)
 # plt.ylim(-0.1, 1.1)
